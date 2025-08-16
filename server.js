8f209d0832ef3ddb7717d5d02b9844b2bafa693f
projete2309-dotcom <projete2309@gmail.com>
2025-08-16 19:02:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ const port = process.env.PORT || 3000;
 // Permite receber JSON no corpo da requisição
 app.use(express.json());
 
-// JSON fixo dentro do código
-let posturaData = { cervical: 0, toracica: 0, lombar: 0 };
-
-app.post('/postura', (req, res) => {
+app.post('/', (req, res) => {
   posturaData = req.body; // atualiza os dados
   console.log('Recebi JSON:');
   console.log(req.body);
