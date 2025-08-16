db2c3354654ef8ef921990f6041e2cdac1ab5734
projete2309-dotcom <projete2309@gmail.com>
2025-08-16 19:53:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 // server.js
 const express = require('express');
-const app = express();
 const cors = require('cors'); 
+const app = express();
 const port = process.env.PORT || 3000;
 
 // Permite receber JSON no corpo da requisição
 app.use(express.json());
 app.use(cors()); // habilita CORS para qualquer origem
 
-app.post('/', (req, res) => {
+// JSON fixo dentro do código
+let posturaData = { cervical: 232, toracica: 434, lombar: 340 };
+
+app.post('/postura', (req, res) => {
   posturaData = req.body; // atualiza os dados
   console.log('Recebi JSON:');
   console.log(req.body);
   res.send('JSON recebido com sucesso!');
 });
-let posturaData = { cervical: 045, toracica: 6534, lombar: 13453 };
+
+
 // Rota para visualizar os dados no navegador
 app.get('/', (req, res) => {
   res.json(posturaData);
 });
+;
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
